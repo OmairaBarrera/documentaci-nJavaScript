4e1bc62377857f2c7233f9128c27f57a8ce4f063
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-07 12:38:22 -0500

    feat: :sparkles: Add method String EndsWith
    The method is from JavaScript, if ends with a character

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-/*Metodo startsWith
-    Si comienza con un caracter.
+/*Metodo endsWith
+    Si finaliza con un caracter.
 */
 
 let cadena = "Cuando te permites lo que mereces, atraes lo que necesitas";
 document.querySelector('.container').innerHTML = `
 
 <h1>Metodos String JavaScript</h1>
-<h2>Metodo StartsWith</h2>
+<h2>Metodo EndsWith</h2>
 ${cadena}<br>
-${cadena.startsWith('J')}
+${cadena.endsWith('s')}
 
 ` 
-console.log(cadena.startsWith('J'));
+console.log(cadena.endsWith('s'));
