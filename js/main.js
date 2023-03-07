ec3a5f7ca7bd55bf96d47b514c72f8b1288ce18e
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-07 12:57:49 -0500

    feat: :sparkles: Add method String Substring
    The method is from JavaScript, extract a section of the string and create a new string.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*Metodo slice
+/*Metodo substring
     extrae seccion de una cadena y devuelve una nueva cadena.
 */
 
@@ -6,9 +6,11 @@ let cadena = "No importa que tan lento vayas mientras no te detengas.";
 document.querySelector('.container').innerHTML = `
 
 <h1>Metodos String JavaScript</h1>
-<h2>Metodo Slice</h2>
+<h2>Metodo Substring</h2>
 ${cadena}<br>
-${cadena.slice(25,50)}
+${cadena.substring(25,45)}<br>
+${cadena.substring(-5,45)}
 
 ` 
-console.log(cadena.slice(25,50));
+console.log(cadena.substring(25,45));
+console.log(cadena.substring(-5,45));
