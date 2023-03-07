3afdb7e3880ce6e66578e21ad72fc86f49c2e3fe
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-07 15:41:44 -0500

    feat: :sparkles: Add method STring charAt
    The method is from JavaScript, char at position

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
-/*Metodo split
-    porciona dependiendo de especificaciones.
+/*Metodo charArt
+    Devuelve el caracter de una posicion en especifico
 */
 
-let cadena = "Dedica tu tiempo a disfrutar la vida porque es eso lo unico que te hara verdaderamente feliz.";
-let cadena2 = "La vida es como un libro.";
+let cadena = "La adversidad y la perseverancia pueden diseñar tu vida";
 document.querySelector('.container').innerHTML = `
 
 <h1>Metodos String JavaScript</h1>
-<h2>Metodo split</h2>
+<h2>Metodo CharArt</h2>
 ${cadena}<br>
-${cadena.split(' ')}<br>
-${cadena.split(' ').length}<br>
-${cadena2}<br>
-${cadena2.split(' ')}<br>
-${cadena2.split(' ').length}
+${cadena.charAt(10)}<br>
+${cadena.charAt(25)}
+
 ` 
-console.log(cadena.split(' '));
-console.log(cadena.split(' ').length);
-console.log(cadena2.split(' '));
-console.log(cadena2.split(' ').length);
+console.log(cadena.charAt(10));
+console.log(cadena.charAt(25));
