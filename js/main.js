fe2c794fbbf39fc5f67030a844923f95b4a647ed
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-07 09:25:38 -0500

    feat: :sparkles: Add method String Length
    The method is from JavaScript, count characteres of the String.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-/**/
+/*Metodo Length
+    Indica el numero de unidades en la cadena
+*/
+
+let cadena = "Inicio de JavaScript con metodos de strings";
+document.querySelector('.container').innerHTML = `
+
+<h1>Metodos String JavaScript</h1>
+<h2>Metodo Length</h2>
+${cadena}<br>
+${cadena.length}
+
+` 
+console.log(cadena.length);
