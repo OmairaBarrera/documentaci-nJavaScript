8670cdb95f031e6167a872d01f4b59dbe546d59b
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-07 12:53:53 -0500

    feat: :sparkles: Add method String Slice
    The method is from JavaScript, extract a section of the string and create a new string.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-/*Metodo replace
-    Busca coincidencia y reemplaza, pero solo la primera.
+/*Metodo slice
+    extrae seccion de una cadena y devuelve una nueva cadena.
 */
 
 let cadena = "No importa que tan lento vayas mientras no te detengas.";
 document.querySelector('.container').innerHTML = `
 
 <h1>Metodos String JavaScript</h1>
-<h2>Metodo Replace</h2>
+<h2>Metodo Slice</h2>
 ${cadena}<br>
-${cadena.replace('lento','rapido')}
+${cadena.slice(25,50)}
 
 ` 
-console.log(cadena.replace('lento','rapido'));
+console.log(cadena.slice(25,50));
