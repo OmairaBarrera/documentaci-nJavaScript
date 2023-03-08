dfca5df486bf256c48c6022dc46cc3d61a77a68c
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-08 08:56:38 -0500

    feat: :sparkles: Add method String lastIndexOf
    The method is from JavaScript, ﬁnd from end.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-/*Metodo indexOf
-    Indica el indice de un substring y desde una posicion indicada
+/*Metodo lastindexOf
+    Indica el ultimo indice de un substring y desde una posicion indicada
 */
 
 let cadena = "Llena tu espiritu de triunfo";
@@ -10,8 +10,8 @@ document.querySelector('.container').innerHTML = `
 <h2>Metodo indexOf</h2>
 ${cadena}<br>
 ${cadena.indexOf('u')}<br>
-${cadena.indexOf('u', 14)}
+${cadena.lastIndexOf('u')}
 
 ` 
 console.log(cadena.indexOf('u'));
-console.log(cadena.indexOf('u', 14));
+console.log(cadena.lastIndexOf('u'));
