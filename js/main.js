dc101a187cdc47d20cc5df4da97f645327c72665
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-09 13:12:47 -0500

    feat: :sparkles: Add Mathod Math min
    This method is about return the smaller number

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-/*Metodos Math.max
-    retorna el mayor de cero o más números dados como parámetros de entrada
+/*Metodos Math.min
+    retorna el menor de cero o más números dados como parámetros de entrada
 */
 
-console.log("Calcular el numero maximo de una serie de numeros")
+console.log("Calcular el numero menor de una serie de numeros")
 
-console.log("Numero mayor de: 1, 3, 2  =  ",Math.max(1, 3, 2));
-console.log("Numero mayor de: -1, -3, -2  =  ",Math.max(-1, -3, -2));
+console.log("Numero menor de: 1, 3, 2  =  ",Math.min(1, 3, 2));
+console.log("Numero menor de: -1, -3, -2  =  ",Math.min(-1, -3, -2));
 
 const array1 = [1, 3, 2];
-console.log("Numero mayor de ",array1)
-console.log(Math.max(...array1));
+console.log("Numero menor de ",array1)
+console.log(Math.min(...array1));
 
 
   
