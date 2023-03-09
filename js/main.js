d0cd513ad85fe6c9f976f09ab5f76cc7a2b02140
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-09 13:32:13 -0500

    feat: :sparkles: Add Method Math pow
    This method is about return exponentiation of a number

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-/*Metodos Math.min
-    retorna el menor de cero o más números dados como parámetros de entrada
+/*Metodos Math.pow
+    devuelve la base elevada al exponente
 */
 
-console.log("Calcular el numero menor de una serie de numeros")
+console.log("Calcular el exponente de un numero")
 
-console.log("Numero menor de: 1, 3, 2  =  ",Math.min(1, 3, 2));
-console.log("Numero menor de: -1, -3, -2  =  ",Math.min(-1, -3, -2));
+console.log("Exponenciacion simple")
+console.log("7 ^ 2 = ", Math.pow(7, 2));   
+console.log("7 ^ 3 = ", Math.pow(7, 3));  
+console.log("2 ^ 10 = ", Math.pow(2, 10));  
 
-const array1 = [1, 3, 2];
-console.log("Numero menor de ",array1)
-console.log(Math.min(...array1));
+console.log("Exponenciacion exponentes fraccionales")
+console.log("4 ^ 0.5 = ", Math.pow(4, 0.5));  // (raíz cuadrada de 4)
+console.log("8 ^ 1/3 = ", Math.pow(8, 1/3));  // (raíz cúbica de 8)
+console.log("2 ^ 0.5 = ", Math.pow(2, 0.5));  // (raíz cuadrada de 2)
+console.log("2 ^ 1/3 = ", Math.pow(2, 1/3));  // (raíz cúbica de 2)
+
+console.log("Exponenciacion exponentes con signo")
+console.log("7 ^ -2 = ", Math.pow(7, -2));   
+console.log("8 ^ -1/3 = ", Math.pow(8, -1/3)); 
+console.log("Exponenciacion bases con signo")
+
+console.log("-7 ^ 2 = ", Math.pow(-7, 2));   // (los cuadrados son positivos)
+console.log("-7 ^ 3 = ", Math.pow(-7, 3));   // (El cubo de una base negativa puede ser negativo)
+console.log("-7 ^ 0.5 = ", Math.pow(-7, 0.5)); // NaN (Los números negativos no tienen raíz cuadrada real)
+console.log("-7 ^ 1/3 = ", Math.pow(-7, 1/3)); // NaN  
+
+
 
 
   
