cf9dcd307b853ff494da1731e9e52ec9171bc585
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-09 09:11:22 -0500

    feat: :sparkles: Add Method Math abs
    This method is about return the absolute value of a number

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-/*Metodos Math.PI
-    representa la relacion entre la longitud de la circunferencia de un circulo y su diametro, la cual es aproximadamente 3.14159.
+/*Metodos Math.abs
+    retorna el valor absoluto de un número
 */
 
 function calculaCircunferencia(radio) {
     return 2 * Math.PI * radio;
 }
 
-console.log("Calcular cicunferencia, teniendo en cuenta que la formula es: 2*π*r, donde  π = ", Math.PI)
-console.log("Calcular Circunferencia, donde el radio es: 1")
-console.log("Circunferencia es: ",calculaCircunferencia(1)); 
-console.log("Calcular Circunferencia, donde el radio es: 4")
-console.log("Circunferencia es: ",calculaCircunferencia(4)); 
+console.log("Calcular el valor absoluto de un numero")
+console.log("Calcular el valor absoluto de: -15")
+console.log("Valor absoluto es: ",Math.abs(-15)); 
+console.log("Calcular el valor absoluto de: 4")
+console.log("Valor absoluto es: ",Math.abs(4)); 
   
 
