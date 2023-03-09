713cff952b5f01eccc4f2e4c81b9a4a5ad6933a0
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-09 07:08:31 -0500

    feat: :sparkles: Add Arithmetic Operators of the JavaScript
    They are the ones we use to perform basic mathematical operations.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-/*
+/* Operadores Aritmeticos
+    son los operadores que utilizamos para realizar operaciones matemáticas básicas
+    1. suma
+    2. resta
+    3. Multiplicacion
+    4. Division
+    5. Modulo
+    6. Exponenciacion
 */
 
 document.querySelector('.container').innerHTML = `
 
 <h1>Operadores Aritmeticos JavaScript</h1>
-<h2></h2>
+<h2>1. Suma</h2>
+<p>La suma de numeros, por ejemplo: <br> 4 + 5 = ${4+5}</p>
+<h2>2. Resta</h2>
+<p>La resta de numeros, por ejemplo: <br> 6 - 4 = ${6-4}</p>
+<h2>3. Multiplicacion</h2>
+<p>La multiplicacion de numeros, por ejemplo: <br> 6 * 4 = ${6*4}</p>
+<h2>4. Division</h2>
+<p>La division de numeros, por ejemplo: <br> 25 / 4 = ${25/4}</p>
+<h2>5. Modulo</h2>
+<p>La division de numeros, pero devuelve el resto de la division, por ejemplo: <br> 25 % 4 = ${6%4}</p>
+<h2>6. Exponenciacion</h2>
+<p>Eleva a la potencia, por ejemplo: <br> 6 ** 4 = ${6**4}</p>
 
 ` 
 
