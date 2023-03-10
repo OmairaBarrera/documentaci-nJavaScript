9d5f93ac656a8da7e103f550bb41d094db6885a9
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-09 21:42:01 -0500

    feat: :sparkles: Add Method Math sqrt
    This method is about return the square root of a number.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
-/*Metodos Math.random
-    Un número de coma flotante pseudo-aleatorio entre los números 0 (incluido) y 1 (excluido)
+/*Metodos Math.sqrt
+    retorna la raíz cuadrada de un número
 */
-console.log("Metodo math.random: devuelve valores entre 0 y 1");
-console.log(Math.random());
-console.log(Math.random());
-console.log(Math.random());
-
-//Ejemplo de una funcion para obtener numeros aleatorios
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-console.log("Funcion para devolver numeros aleatorios de un rango maximo")
-console.log("Numero aleatorio entre 0 y 3")
-console.log(getRandomInt(3));
-console.log("Numero aleatorio entre 0 y 10")
-console.log(getRandomInt(10));  
-
+console.log("La raiz cuadrada de varios numeros");
+console.log("La raiz cuadrada de 9 = ", Math.sqrt(9));
+console.log("La raiz cuadrada de 2 = ", Math.sqrt(2)); 
+console.log("La raiz cuadrada de numeros negativos");
+console.log("La raiz cuadrada de -1 = ", Math.sqrt(-1));
 
 
 
