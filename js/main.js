2d6fe68222c33f832e14b7669be94c23553545a5
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-09 22:04:41 -0500

    feat: :sparkles: Add Method Math floor
    This method is about returns the nearest integer greater than or equal to a given number..

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-/*Metodos Math.ceil
-    devuelve el entero mayor o igual más próximo a un número dado
+/*Metodos Math.floor
+    Devuelve el máximo entero menor o igual a un número.
 */
 console.log("Valor mas proximo de varios numeros");
-console.log("Numero mas cercano a .95 = ", Math.ceil(.95));
-console.log("Numero mas cercano a 4 = ", Math.ceil(4)); 
-console.log("Numero mas cercano a 7.004 = ", Math.ceil(7.004));
-console.log("Numero mas cercano a -7.004 = ", Math.ceil(-7.004));
+console.log("Numero mas cercano a .95 = ", Math.floor(.95));
+console.log("Numero mas cercano a 4 = ", Math.floor(4)); 
+console.log("Numero mas cercano a 7.004 = ", Math.floor(7.004));
+console.log("Numero mas cercano a -7.004 = ", Math.floor(-7.004));
 
 
 
