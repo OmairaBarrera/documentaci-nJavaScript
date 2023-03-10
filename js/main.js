06365983f2d5eff559be460d20da0edec4c0f192
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-09 20:49:25 -0500

    feat: :sparkles: Add Method Math random
    The method is about return a random number of zero - one.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
-/*Metodos Math.pow
-    devuelve la base elevada al exponente
+/*Metodos Math.random
+    Un número de coma flotante pseudo-aleatorio entre los números 0 (incluido) y 1 (excluido)
 */
+console.log("Metodo math.random: devuelve valores entre 0 y 1");
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
 
-console.log("Calcular el exponente de un numero")
+//Ejemplo de una funcion para obtener numeros aleatorios
 
-console.log("Exponenciacion simple")
-console.log("7 ^ 2 = ", Math.pow(7, 2));   
-console.log("7 ^ 3 = ", Math.pow(7, 3));  
-console.log("2 ^ 10 = ", Math.pow(2, 10));  
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
-console.log("Exponenciacion exponentes fraccionales")
-console.log("4 ^ 0.5 = ", Math.pow(4, 0.5));  // (raíz cuadrada de 4)
-console.log("8 ^ 1/3 = ", Math.pow(8, 1/3));  // (raíz cúbica de 8)
-console.log("2 ^ 0.5 = ", Math.pow(2, 0.5));  // (raíz cuadrada de 2)
-console.log("2 ^ 1/3 = ", Math.pow(2, 1/3));  // (raíz cúbica de 2)
+console.log("Funcion para devolver numeros aleatorios de un rango maximo")
+console.log("Numero aleatorio entre 0 y 3")
+console.log(getRandomInt(3));
+console.log("Numero aleatorio entre 0 y 10")
+console.log(getRandomInt(10));  
 
-console.log("Exponenciacion exponentes con signo")
-console.log("7 ^ -2 = ", Math.pow(7, -2));   
-console.log("8 ^ -1/3 = ", Math.pow(8, -1/3)); 
-console.log("Exponenciacion bases con signo")
 
-console.log("-7 ^ 2 = ", Math.pow(-7, 2));   // (los cuadrados son positivos)
-console.log("-7 ^ 3 = ", Math.pow(-7, 3));   // (El cubo de una base negativa puede ser negativo)
-console.log("-7 ^ 0.5 = ", Math.pow(-7, 0.5)); // NaN (Los números negativos no tienen raíz cuadrada real)
-console.log("-7 ^ 1/3 = ", Math.pow(-7, 1/3)); // NaN  
+
 
 
 
