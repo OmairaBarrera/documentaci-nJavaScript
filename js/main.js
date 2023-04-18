7af13b84cc35c67b79e10d08e4c8582125b34d89
OmairaBarrera <lomaira.barrera@gmail.com>
2023-04-18 08:29:20 -0500

    feat: :rocket: Config workspace
    Begin topic APIs

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
-/* Clausuras
-una clausura o cierre se define como una función que «encierra» 
-variables en su propio ámbito (y que continúan existiendo aún 
-    habiendo terminado la función)
+/* 
+
 */
 
-// Clausura: Función incr()
-const incr = (function () {
-    let num = 0;
-    return function () {
-      num++;
-      return num;
-    };
-  })();
-  
-  typeof incr; // 'function'
-  incr(); // 1
-  incr(); // 2
-  incr(); // 3
 
 
 
